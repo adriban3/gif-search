7cbfb3f06dd395e3311666600825c143ee0484d0
Andrew Driban <dribaan@habasitusa.net>
2018-06-20 11:08:29 -0400

    psuedocoded semi-adequately, start coding and figure out what's wrong

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,50 @@
 //this url uses "search" endpoint on giphy api (search?)
 //search criteria include q (search terms), limit (number of gifs shown, should be 10 for this assignment?), rating (leave this out ideally, display this under gif though?), lang (prob just set to english, or leave out, idk)
 
+//set up app as object
+
+//api key property
+
+//url property
+
+//TV show array property
+
+//-------------function 1------------------
+
+//for loop to add array elements to screen (for each element in array, append new button with array text to div)
+
+//----------function 2---------------------
+
+//create url based on search criteria
+
+//call function 3
+
+//------------function 3-------------------
+
+//run ajax call with apikey property and newly edited url
+//call function 5
+
+//=---------function 4--------------------
+
+//push users input from text box to tv show array
+
+//call function 1
+
+//---------function 5----------------------
+
+//display gifs on screen
+//gifs should be displayed static until click (click event below)
+//make sure to prepend as gifs should not be replaced?
+//also display ratings under each gif
+
+//----------------Click events (defined outside of functions)--------------------
+
+//on GIF TOPIC button click, function 2
+
+//on SEARCH button click, function 4
+
+//on gif click, animate/staticate(?) gifs (essentially change state from active to inactive or inactive to active)
+
+//----------------on startup----------------------
+
+//run function 1
