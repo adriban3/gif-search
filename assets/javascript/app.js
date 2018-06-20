eed5cc04468ac1201a06c3f276a98535bfb3fc6b
Andrew Driban <dribaan@habasitusa.net>
2018-06-20 10:29:17 -0400

    added comments to js with example url and api key, no code yet

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//API key = W0NHYrDzcv0pFJplSchuDzX9s4R4tnG5
+
+//url = https://api.giphy.com/v1/gifs/search?api_key=W0NHYrDzcv0pFJplSchuDzX9s4R4tnG5&q=the office&limit=25&offset=0&rating=Y&lang=en
+
+//this url uses "search" endpoint on giphy api (search?)
+//search criteria include q (search terms), limit (number of gifs shown, should be 10 for this assignment?), rating (leave this out ideally, display this under gif though?), lang (prob just set to english, or leave out, idk)
+
